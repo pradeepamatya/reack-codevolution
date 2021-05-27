ddf0607268519997bb4bcc1bbfdbd1002489a53d
pradeepamatya <pradeepbdramatya@gmail.com>
2021-05-27 14:40:56 +0545

    first commit

---
+++ components/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hello = () => {
+
+    return (
+        <div className='dummyClass'>
+        <h1>Hello vishwas</h1>  
+        </div>
+    )
+}
+
+export default Hello
+
+
